a897c50c64ba6bb580a3a47efff0c8dfbd56f2da
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-11 18:29:16 +0530

    Changes

--- index.d.ts
+++ index.d.ts
@@ -49,3 +49,45 @@ declare function info(...args: any[]): void;
  * // Logs: "YYYY-MM-DD HH:MM:SS.MMM AM/PM [info] >> [ 'This is a log message' ] << [info]"
  */
 declare function trace(...args: any[]): void;
+
+/**
+ * Executes a callback function and handles any errors that occur during its execution.
+ * @param test - The callback function that contains the code to be tested.
+ * @param args - Additional arguments that can be passed to the callback function.
+ * @returns void
+ */
+declare function handleError(test: () => void, ...args: any[]): void;
+
+/** 
+ * @satisfies the following Generic class and its functions.
+ * @belongs to class and its subsidiary functions.
+ */
+declare interface KeyValuePair<K, V> {
+    key: K;
+    value: V;
+}
+declare class Generic{
+    constructor();
+    /**
+     * Generic function to create a KeyValuePair asynchronously.
+     * 
+     * @param key - The key of the KeyValuePair.
+     * @param value - The value of the KeyValuePair.
+     * @returns A Promise that resolves to a KeyValuePair object.
+     * @example
+     * const numberStringPair = await createKeyValuePair<number, string>(1, "TypeScript");
+     * numberStringPair is { key: 1, value: "TypeScript" }
+
+     * const stringBooleanPair = await createKeyValuePair<string, boolean>("isTrue", true);
+     * stringBooleanPair is { key: "isTrue", value: true }
+     */
+    public createKeyValuePair<K, V>(key: K, value: V): Promise<KeyValuePair<K, V>>;
+    /**
+     * Creates a key-value pair using the provided key and value.
+     * It is done via using Generic Programming.
+     * @param key - The key for the key-value pair.
+     * @param value - The value for the key-value pair.
+     * @returns A Promise that resolves to void.
+     */
+    public CreateKeyValuePair(key: any, value: any): Promise<void>;
+}
