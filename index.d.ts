64f4c70fd953aa6f006cdfe2a5577d1518b208f8
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-11 17:33:32 +0530

    ~

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+/** 
+* @copyright Copyright (c) by Vedansh <admin@override.ps1>. All rights reserved.
+*/
+declare class Stack<T>{
+    /**  
+    * @var elements is an array of elements of type T.
+    */
+    private elements: T[];
+
+    /**  
+    * @var StackName is an static member of class Stack<T> that stores the name of the stack.
+    */
+    static StackName: string | undefined;
+
+    /**
+     * Adds an element to the top of the stack.
+     * @param element The element to be added.
+     */
+    push(element: T): void;
+
+    /**
+     * Removes and returns the element at the top of the stack, or returns `undefined` if the stack is empty.
+     * @returns The element at the top of the stack, or `undefined` if the stack is empty.
+     */
+    pop(): T | undefined;
+
+    /**
+     * Checks if the stack is empty.
+     * @returns `true` if the stack is empty, `false` otherwise.
+     */
+    isEmpty(): boolean;
+}
