e0bc1ac3e2147d57f6d1d94624c789fb3e019b26
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-31 15:05:18 +0530

    Fixes

--- test/script.ts
+++ test/script.ts
@@ -1,2 +1,15 @@
-import * as a from 'override.ps1';
-a.info();
+import * as over from 'override.ps1';
+
+// stdout to console.
+over.trace("This is a trace message.");
+
+// If you prefer Generic Programming.
+const A = [2, 5, 6];
+const R = over.Permute<number>(A);
+
+over.trace("All Permutations: ");
+R.forEach((permutation) => {
+    console.log(permutation);
+});
+
+over.trace(" == End of Script == ");
