74f3008da43557c89d0bfe256ae6a549e2ce3e67
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-04 13:50:28 +0530

    `getValue()`

--- index.ts
+++ index.ts
@@ -120,7 +120,34 @@ export function Permute<T>(arr: T[]): T[][] {
 export function Swap<T>(arr: T[], i: number, j: number) {
     [arr[i], arr[j]] = [arr[j], arr[i]];
 }
+/**
+ * Retrieves the value corresponding to a specified key from an object.
+ * If the key does not exist in the object, it returns the key itself.
+ * @author Vedansh ✨.
+ * @template TObj - The type of the object.
+ * @template TKey - The type of the key.
+ * @param {TObj} obj - The object from which to retrieve the value.
+ * @param {TKey} key - The key to check and retrieve the value for.
+ * @param {...TKey[]} args - Additional keys to check and retrieve the values for.
+ * @returns {TKey | TObj[TKey]} - The value corresponding to the specified key if it exists in the object,
+ * otherwise the key itself.
+ */
 
+export function getValue<TObj, TKey extends keyof TObj>(
+        obj: TObj, key: TKey, ...args: Array<TKey>
+    ): TKey | TObj[TKey] {
+    if (!obj?.hasOwnProperty(key)) {
+        console.log(`Error ! The Object Doesn't Consist The Key: ${key as string}\n`);
+        return key;
+    }
+    for (const arg of args) {
+        if (!obj.hasOwnProperty(arg)) {
+            console.log(`OOPS ! The Object Doesn't Consist The Key: ${arg as string}\n`);
+            return arg;
+        }
+    }
+    return obj[key] as TKey | TObj[TKey];
+}
 /** 
  * @satisfies the following Generic class and its functions.
  * @belongs to class and its subsidiary functions.
