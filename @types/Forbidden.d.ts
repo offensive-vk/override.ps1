5962857cb467d24137267b618d04b42fb041b4d0
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-15 13:41:32 +0530

    Forbidden = false

---
+++ @types/Forbidden.d.ts
@@ -0,0 +1,12 @@
+export declare namespace SRC {
+    async function fetchPath() : Promise<void> {
+        throw DOMException.NAMESPACE_ERR
+    }
+    export declare class Module<M> {
+        constructor(
+            private List: Stack<>,
+            private Private_List: HashMap<never>,
+            private Public_List: HashSet<number>, 
+        ) { }
+    }
+}
