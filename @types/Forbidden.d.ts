08d29b4666c145125ddb713c77cf817bb46173cb
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-26 11:52:06 +0530

    Forbidden

--- @types/Forbidden.d.ts
+++ @types/Forbidden.d.ts
@@ -1,3 +1,5 @@
+import { Hash } from "crypto";
+
 export declare namespace SRC {
     async function fetchPath() : Promise<void> {
         throw DOMException.NAMESPACE_ERR
@@ -8,5 +10,15 @@ export declare namespace SRC {
             private Private_List: HashMap<never>,
             private Public_List: HashSet<number>, 
         ) { }
+
+        private getList(L: Stack<>): ReturnType<List> {
+            console.log(`Fetching List....${L}`);
+            return this.List;
+        }
+
+        private getPrivateList(L: HashMap<number>): Hash<never> | any {
+            if(L instanceof Module) { }
+            return null;
+        }
     }
 }
